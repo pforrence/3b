fc8a3ec7e5709c05dd64e102e38ccc45985d5bae
nep36 <nep36@pitt.edu>
2018-04-07 17:49:00 -0400

    Auxiliary Files

---
+++ prf8-assignment3/helper_files/Helper.java
@@ -0,0 +1,18 @@
+package helper_files;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.Collection;
+
+public class Helper
+{
+    public static List<String> keysToSortedList(Collection<String> collection)
+    {
+        List<String> list = new ArrayList<String>(collection);
+        
+        java.util.Collections.sort(list);
+        
+        return list;
+    }
+}
